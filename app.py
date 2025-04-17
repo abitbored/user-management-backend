02269bdfd2167331d0e32515f387da7b9e5f4927
Abi <abimanyupalagan@gmail.com>
2025-04-17 17:25:34 +0700

    remove things

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///horus_abi_db.sqlite"
-app.config["SECRET_KEY"] = "5f79c46a481032f81e06204ccd0312c3"
-app.config["JWT_SECRET_KEY"] = "f97927a6d7bed5e65c3e22cba39033216b190ef8f01984467d4e374b759c8dcb"
+app.config["SECRET_KEY"] = "your_secret_key"
+app.config["JWT_SECRET_KEY"] = "your_secret_key"
 
 db = SQLAlchemy()
 db.init_app(app)
